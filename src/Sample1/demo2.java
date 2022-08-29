46dab13dc23ba7dbf27dadedd299777592825cb4
Parvin <Parvin.makandar177@gmail.com>
2022-08-29 17:28:07 +0530

    [Tanvir] : 2nd changes in project

---
+++ src/Sample1/demo2.java
@@ -0,0 +1,20 @@
+package Sample1;
+
+public class demo2 
+{
+
+	public static void main(String[] args)
+	{
+		
+	  System.out.println("Tanvir");
+	
+	
+	
+	
+	  System.out.println("makandar");
+	
+	
+	
+	}
+	
+}
