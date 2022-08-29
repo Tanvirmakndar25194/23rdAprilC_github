dd8071b9cd42fcd7d4b5c055f34059aefbc5a596
Parvin <Parvin.makandar177@gmail.com>
2022-08-29 20:48:38 +0530

    [tanvir]:  3rd changes in project

---
+++ src/Sample1/demo3.java
@@ -0,0 +1,23 @@
+package Sample1;
+
+public class demo3
+{
+
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("tanvir");
+		
+		
+		System.out.println("parveen");
+		
+		
+		System.out.println("afreen");
+		
+		
+		
+		
+	}
+	
+}
